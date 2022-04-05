5c0b393a8546f5ecfef8a2d33cb2867e3c4d3891
Brady Ash <bradyash9@gmail.com>
2022-04-04 19:59:09 -0500

    Product and Inventory Class with csv file input support.

--- inventory/src/DisplayInventory.java
+++ inventory/src/DisplayInventory.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class DisplayInventory {
+    public void display(Inventory inv) {
+        ArrayList<Product> inventory = inv.inventory;
+        for(Product p: inventory) {
+            System.out.printf("%20s %8s %s", p.getName(), p.getPrice(), p.getDescription());
+        }
+    }
 }
--- inventory/src/Product.java
+++ inventory/src/Product.java
@@ -1,2 +1,32 @@
 public class Product {
+    String name;
+    double price;
+    String description;
+    Product() {
+        name = "No Name Set";
+        price = 0.00;
+        description = "No Description Set";
+
+    }
+    public void setName(String s) {
+        name = s;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double p) {
+        price = p;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setDescription(String s) {
+        description = s;
+    }
+    public String getDescription() {
+        return description;
+    }
+
 }
