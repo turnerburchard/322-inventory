7874a6be51e5a9e29c52ed07e736485716dd5b47
ezra <ezraskoog@yahoo.com>
2022-04-05 19:23:24 -0600

    Throw error message when duplicate item name is entered.

--- prototype/src/Inventory.java
+++ prototype/src/Inventory.java
@@ -11,6 +11,22 @@ public class Inventory {
         products.removeIf(e -> e.getID().equals(id));
     }
 
+    /***
+     * Checks if the new product's name is already contained in the list of products.
+     * Returns FALSE if the product name does not exist
+     * Returns TRUE if the product name already exist
+     ***/
+    public boolean contains(String newProduct) {
+
+        for (Product product : products) {
+            String productName = product.getName();
+            if (productName.equals(newProduct)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public String toString(){
         String out = "";
         for (Product p:products){
