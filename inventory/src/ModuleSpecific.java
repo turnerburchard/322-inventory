70e78fb098f47f814d4c4d60d6fa923e32a04c6b
Jeremy Heng <jeremy@heng.org>
2022-04-13 17:40:05 -0600

    trying to fix issues with pull request

--- inventory/src/ModuleSpecific.java
+++
@@ -1,8 +0,0 @@
-public interface ModuleSpecific { // for Modules children, methods that are needed but must be specific to a module
-
-    void driver(); // drives functionality of module
-
-    void userOptions(); // presents options available to user during the module
-
-    void userInput(); // processes user input specific to module
-}
--- inventory/src/Modules.java
+++
@@ -1,6 +0,0 @@
-public abstract class Modules { // abstract class used to create children that hold system functions
-
-    public void returnToMain() { // to return to UserInterface (main menu) from a module
-
-    }
-}
