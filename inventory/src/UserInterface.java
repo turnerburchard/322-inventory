e1e5f9550c41a3b16f230f1b2a0b6c97b8221d83
Jeremy Heng <jeremy@heng.org>
2022-04-13 15:05:08 -0600

    testing github/framework additions
    -making sure I know how git works
    -some framework based on design diagram

--- inventory/src/UserInterface.java
+++ inventory/src/UserInterface.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class UserInterface {
+    private Modules funcType = null; // current module/module to be used, Jeremy can explain
+
+    public static void main(String[] args) throws FileNotFoundException { //!!!! prolly needs changes, will want to combine with menu()
+        Product p = new Product();
+        p.setName("Turner's Fantastic Stir Fry and Cherry Pie");
+        p.setPrice(19.99);
+        p.setDescription("This Stir Fry and Cherry Pie evokes memories of the salty breeze off the ocean, and a warm summer day");
+        //System.out.println(p.getName() + " \nPrice: " + p.getPrice() + " \nDescription: " + p.getDescription());
+
+        File data = new File("data.csv");
+        Inventory inventory = new Inventory();
+        inventory.addProduct(p);
+        inventory.importProducts(data);
+        DisplayInventory.display(inventory);
+        UserInterface.menu();
+
+    }
+
+    public void changeFuncType() {
+
+    }
+
     public static void menu() {
         System.out.println("Menu:\nEnter 1 to add a product\nEnter 2 to search for a product\nEnter 3 to modify an object\nEnter 0 to exit");
         Scanner inp = new Scanner(System.in);
