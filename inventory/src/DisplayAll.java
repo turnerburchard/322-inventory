ddb5d0903417c3d06ed6af9cdef11585e922f171
Jeremy Heng <jeremy@heng.org>
2022-04-16 11:22:30 -0600

    misc edits
    - created DisplayAll function module class
    - changed UserInterface to actually load Products on start

---
+++ inventory/src/DisplayAll.java
@@ -0,0 +1,14 @@
+public class DisplayAll extends Modules{ // for displaying all Products in inventory
+
+    public void moduleDriver() {
+        
+    }
+
+    public void userOptions() {
+
+    }
+
+    public void userInput() {
+
+    }
+}
