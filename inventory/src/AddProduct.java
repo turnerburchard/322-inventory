d7f37c232c4fef8690c686f02561bf7ab33fd77e
Friedonuts32 <98046392+Friedonuts32@users.noreply.github.com>
2022-04-16 16:27:47 -0600

    EditProduct class
    fixed EditProduct class
    - added comments to AddProduct as well

--- inventory/src/AddProduct.java
+++ inventory/src/AddProduct.java
@@ -1,9 +1,9 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class AddProduct extends Modules {
+public class AddProduct extends Modules { // System function, adds product to inventory
 
-    public void moduleDriver() throws IOException {
+    public void moduleDriver() throws IOException { // drives module function
         userInput();
         try {
             returnToMain();
@@ -13,7 +13,7 @@ public class AddProduct extends Modules {
 
     }
 
-    public void userOptions() throws IOException {
+    public void userOptions() throws IOException { // displays menu for user
         System.out.println("1 -> Add Product " + "\n2 -> Exit to Main Menu"); // Menu
         Scanner str = new Scanner(System.in); // creates scanner object
         String inputString = str.nextLine(); // takes whatever user enters, so as not to create error
@@ -26,7 +26,7 @@ public class AddProduct extends Modules {
 
     }
 
-    public void userInput() throws IOException {
+    public void userInput() throws IOException { // handles module's input
         Scanner n = new Scanner(System.in);
         Scanner p = new Scanner(System.in);
         Scanner d = new Scanner(System.in);
@@ -34,6 +34,8 @@ public class AddProduct extends Modules {
         Scanner a = new Scanner(System.in);
         Scanner i = new Scanner(System.in);
 
+        System.out.println(">>>> Enter ID: "); // take id from user
+        String id = i.nextLine();
 
         System.out.println(">>>> Enter Name: ");
         String name = n.nextLine(); // take name from user
@@ -44,7 +46,7 @@ public class AddProduct extends Modules {
             price = p.nextDouble(); // take price from user
         }
         catch(Exception e){ // if input is not a double
-            System.out.println("====| WRONG INPUT: Try Again!");
+            System.out.println("====| WRONG INPUT: ");
             userOptions();
             return;
         }
@@ -59,7 +61,7 @@ public class AddProduct extends Modules {
             stock = s.nextInt(); // take stock from user
         }
         catch(Exception e){ // if input is not an int
-            System.out.println("====| WRONG INPUT: Try Again!");
+            System.out.println("====| WRONG INPUT: ");
             userOptions();
             return;
         }
@@ -70,13 +72,10 @@ public class AddProduct extends Modules {
             active = a.nextBoolean(); // take active from user
         }
         catch(Exception e){ // if input is not a boolean
-            System.out.println("====| WRONG INPUT: Try Again!");
+            System.out.println("====| WRONG INPUT: ");
             userOptions();
             return;
         }
-
-        System.out.println(">>>> Enter ID: "); // take id from user
-        String id = i.nextLine();
 
         Product prod = new Product(id); // set variables for the product
         prod.setName(name);
@@ -89,7 +88,7 @@ public class AddProduct extends Modules {
         boolean add = inventory.addProductSec(prod); // add the product
 
         if(add == false){ // if addProductSec can't add a product
-            System.out.println("====| Product " + name + " was not entered successfully");
+            System.out.println("====| ERROR: Product " + name + " already exist");
         }
         else{ // product was added successfully
             System.out.println("====| Product " + name + " was entered successfully");
